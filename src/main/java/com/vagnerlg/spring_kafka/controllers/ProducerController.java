94f9afe70468e2708336cf517827a37c10021ad5
Vagner Goncalves <vlgonalves@gmail.com>
2025-04-11 20:59:35 -0300

    feat(kafka): created simple producer

--- src/main/java/com/vagnerlg/spring_kafka/controllers/ProducerController.java
+++ src/main/java/com/vagnerlg/spring_kafka/controllers/ProducerController.java
@@ -2,6 +2,7 @@ package com.vagnerlg.spring_kafka.controllers;
 
 
 import com.vagnerlg.spring_kafka.controllers.request.RequestMessage;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProducerController {
 
+    private final KafkaTemplate<String, String> template;
+
+    public ProducerController(KafkaTemplate<String, String> template) {
+        this.template = template;
+    }
+
     @PostMapping(path = "/api/producer")
     public String message(@RequestBody RequestMessage requestMessage) {
+        template.send("topic_one", requestMessage.getKey(), requestMessage.getMessage());
         return requestMessage.getMessage();
     }
 }
--- src/main/java/com/vagnerlg/spring_kafka/controllers/request/RequestMessage.java
+++ src/main/java/com/vagnerlg/spring_kafka/controllers/request/RequestMessage.java
@@ -1,8 +1,13 @@
 package com.vagnerlg.spring_kafka.controllers.request;
 
 import lombok.Data;
+import org.springframework.lang.NonNull;
 
 @Data
 public class RequestMessage {
+    @NonNull
+    private String key;
+
+    @NonNull
     private String message;
 }
